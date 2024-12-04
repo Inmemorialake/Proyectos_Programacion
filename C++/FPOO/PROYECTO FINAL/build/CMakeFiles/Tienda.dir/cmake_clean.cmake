1e79941ff90c689bc79c7564d8d8db45aaa600f5
Inmemorialake <inmemorialake2@gmail.com>
2024-12-04 06:11:09 -0500

    Add my final project to github

---
+++ C++/FPOO/PROYECTO FINAL/build/CMakeFiles/Tienda.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tienda.dir/link.d"
+  "CMakeFiles/Tienda.dir/src/barca.cpp.o"
+  "CMakeFiles/Tienda.dir/src/barca.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/conejo.cpp.o"
+  "CMakeFiles/Tienda.dir/src/conejo.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/individuo.cpp.o"
+  "CMakeFiles/Tienda.dir/src/individuo.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/jugador.cpp.o"
+  "CMakeFiles/Tienda.dir/src/jugador.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/lechuga.cpp.o"
+  "CMakeFiles/Tienda.dir/src/lechuga.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/logica.cpp.o"
+  "CMakeFiles/Tienda.dir/src/logica.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/lugar.cpp.o"
+  "CMakeFiles/Tienda.dir/src/lugar.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/main.cpp.o"
+  "CMakeFiles/Tienda.dir/src/main.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/orilla.cpp.o"
+  "CMakeFiles/Tienda.dir/src/orilla.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/robot.cpp.o"
+  "CMakeFiles/Tienda.dir/src/robot.cpp.o.d"
+  "CMakeFiles/Tienda.dir/src/zorro.cpp.o"
+  "CMakeFiles/Tienda.dir/src/zorro.cpp.o.d"
+  "Tienda"
+  "Tienda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tienda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
