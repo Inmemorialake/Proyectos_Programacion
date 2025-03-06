870ff6d16cc5187d84aee80092a1826a8a79a35a
Inmemorialake <andresgerardonzales@gmail.com>
2025-03-06 15:15:37 -0500

    Ahi vamos con POE

--- Java/Sesion 4/src/main/java/com/example/sesion4/Main.java
+++ Java/Sesion 4/src/main/java/com/example/sesion4/Main.java
@@ -1,31 +1,17 @@
 package com.example.sesion4;
 
-import com.example.sesion4.model.Cat;
-import com.example.sesion4.model.Dog;
-import com.example.sesion4.model.Smartphone;
+import com.example.sesion4.view.HomeStage;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
-        //launch(args);
-        Dog dog = new Dog();
-        dog.makeSound();
-
-        Cat cat = new Cat();
-        cat.makeSound();
-
-        Smartphone smartphone = new Smartphone();
-        smartphone.getAproximateCoordinates();
-        smartphone.getCoordinates();
-        smartphone.nextMusicPlayer();
-        smartphone.nextRadio();
+        launch(args);
     }
 
-
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start (Stage primaryStage) {
+        new HomeStage();
     }
 }
--- Java/Sesion 4/src/main/java/com/example/sesion4/view/HomeStage.java
+++ Java/Sesion 4/src/main/java/com/example/sesion4/view/HomeStage.java
@@ -1,6 +1,45 @@
 package com.example.sesion4.view;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class HomeStage extends Stage {
+    private VBox root;
+    private Scene scene;
+
+    public HomeStage() {
+        root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(10);
+        scene = new Scene(root, 512, 256);
+        initStage();
+        ShowHome();
+    }
+
+    private void initStage() {
+        setTitle("Miniproyecto 1");
+        setResizable(false);
+        getIcons().add(new Image(String.valueOf(getClass().getResource("/com/example/sesion4/favicon.png"  ))));
+        setScene(scene);
+        show();
+    }
+
+    private void ShowHome() {
+        Label tittleLabel = new Label("Digite la palabra secreta");
+        tittleLabel.setTextFill(Color.MIDNIGHTBLUE);
+        tittleLabel.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 16));
+        TextField secretWordTextField = new TextField();
+        Button startButton = new Button("Iniciar");
+        root.getChildren().addAll(tittleLabel, secretWordTextField, startButton);
+    }
 }
