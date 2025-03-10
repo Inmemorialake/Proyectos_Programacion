29699e09a255c48a5c3adb4f6a8612eff0267602
Inmemorialake <andresgerardonzales@gmail.com>
2025-03-10 14:08:52 -0500

    Algunos arreglos previos a empezar el Miniproyecto 1 de FPOE

---
+++ Java/Project 1 FPOE/src/main/java/com/project1/project1/Main.java
@@ -0,0 +1,22 @@
+package com.project1.project1;
+import com.project1.project1.model.GameStage;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Scanner;
+
+public class Main extends Application {
+    static String SecretWord = "";
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese la palabra secreta (Palabra a adivinar): ");
+        SecretWord = new Scanner(System.in).nextLine();
+        GameStage gameStage = new GameStage(SecretWord);
+        gameStage.StartGame();
+        System.out.println("Feliz dia!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    }
+}
