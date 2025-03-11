e72e2bee188e4188e2d2b9657ddd3c9c6542732c
Inmemorialake <andresgerardonzales@gmail.com>
2025-03-11 16:41:18 -0500

    Por fin quedo ready la aplicación de consola (Bien chingona) ahora solo falta agregar JavaFx

--- Java/Project 1 FPOE/src/main/java/com/project1/project1/model/GameStage.java
+++ Java/Project 1 FPOE/src/main/java/com/project1/project1/model/GameStage.java
@@ -5,60 +5,41 @@ import java.util.Scanner;
 public class GameStage {
     private final String SecretWord;
     private int Attempts;
-    private int counter;
-    private int LastPosition;
     private boolean[] LetterFound;
+    private Scanner scanner = new Scanner(System.in);
+
 
     public GameStage(String secretWord) {
         SecretWord = secretWord;
         Attempts = 8;
-        counter = 8;
-        LastPosition = 0;
         LetterFound = new boolean[SecretWord.length()];
     }
 
     public void StartGame() {
         System.out.println("Perfecto! Ahora adivina la palabra secreta: ");
-        String word = new Scanner(System.in).nextLine();
-        while (!LetterFound[SecretWord.length()-1]) {
-            if (Attempts == 0){
-                System.out.println("Perdiste! La palabra secreta era: " + SecretWord);
-                break;
-            }
-            else {
-                if (LastPosition == 0) {
-                    checkLetters(word);
-                }
-                else {
-                    word = AskInNewAttempt();
-                    checkLetters(word);
-                }
-            }
-
+        String word = scanner.nextLine().toLowerCase();
+        while (!word.equals(SecretWord) && Attempts > 1) {
+            checkLetters(word);
+            word = AskInNewAttempt();
+        }
+        if (word.equals(SecretWord)) {
+            System.out.println("Felicidades! Adivinaste la palabra secreta: " + SecretWord);
+        }
+        if (Attempts == 0) {
+            System.out.println("Perdiste! La palabra secreta era: " + SecretWord);
         }
     }
 
     public void checkLetters(String word) {
-        if (Attempts == counter) {
-            for (int i = LastPosition; i < SecretWord.length(); i++) {
-                if (SecretWord.charAt(i) == word.charAt(i)) {
-                    LetterFound[i] = true;
-                } else {
-                    --Attempts;
-                    System.out.println("Letra incorrecta en la posición " + (i + 1) + ", intentos restantes: " + Attempts);
-                    LastPosition = i;
-                    break;
-                }
+        for (int i = 0; i < SecretWord.length(); i++) {
+            if (SecretWord.charAt(i) == word.charAt(i)) {
+                LetterFound[i] = true;
             }
-        }
-        else {
-            word = AskInNewAttempt();
-            --counter;
-            checkLetters(word);
         }
     }
 
-    public String AskInNewAttempt() {
+    public String AskInNewAttempt(){
+        Attempts--;
         System.out.println("Tus aciertos fueron:");
         for (int i = 0; i < SecretWord.length(); i++) {
             if (LetterFound[i]) {
@@ -68,8 +49,9 @@ public class GameStage {
                 System.out.print("_");
             }
         }
-        System.out.println("\nIngrese lo que falta de la palabra: ");
-        return new Scanner(System.in).nextLine();
+        System.out.println("\nIntentos restantes: " + Attempts);
+        System.out.println("Ingrese lo que falta de la palabra: ");
+        return scanner.nextLine().toLowerCase();
     }
 }
 
