829be8f413ce65ca2f00f5800f1823c31841c7cb
Inmemorialake <andresgerardonzales@gmail.com>
2025-03-11 17:11:27 -0500

    Avanzando en el proyecto con JavaFx

---
+++ Java/Project 1 FPOE/src/main/java/com/project1/project1/controller/StageController.java
@@ -0,0 +1,4 @@
+package com.project1.project1.controller;
+
+public class StageController {
+}
--- Java/Project 1 FPOE/src/main/java/com/project1/project1/view/StartScreen.java
+++ Java/Project 1 FPOE/src/main/java/com/project1/project1/view/StartScreen.java
@@ -1,4 +1,16 @@
 package com.project1.project1.view;
 
-public class StartScreen {
+import com.project1.project1.controller.StageController;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class StartScreen extends Stage {
+    private VBox root;
+    private Scene scene;
+    private TextField secretWordTextField;
+    private Button playButton;
+    private StageController stageController;
 }
