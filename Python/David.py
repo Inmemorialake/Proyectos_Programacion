c6a8904e2419680c7db4ab69179d27610a7dc9c0
Inmemorialake <inmemorialake2@gmail.com>
2025-05-09 19:38:34 -0500

    Add Proxys

--- Python/David.py
+++ Python/David.py
@@ -10,8 +10,39 @@ import time
 # The list of proxies that you can use
 # You can add your own proxies here
 proxies = [
+    "181.119.84.212:8080",
+    "200.6.178.61:8080",
+    "45.167.126.37:999",
+    "200.110.173.17:999",
+    "200.6.178.20:9992",
+    "181.78.84.79:999",
+    "38.191.146.7:999",
+    "200.6.178.21:9992",
+    "190.6.162.3:8080",
+    "45.167.126.105:999",
+    "181.129.252.123:999",
+    "38.199.30.195:999",
+    "200.6.178.21:999",
+    "201.185.42.139:11201",
+    "181.204.183.251:999",
+    "200.6.176.58:9991",
+    "200.6.176.62:9992",
+    "200.116.198.222:9812",
+    "181.119.86.188:999",
+    "181.119.86.186:999",
+    "200.6.176.58:9992",
+    "38.199.30.173:999",
+    "38.191.209.202:999",
+    "181.119.84.87:999",
+    "204.199.81.94:999",
+    "200.6.179.53:999",
+    "181.129.250.46:999",
+    "181.52.238.165:54694",
+    "45.167.126.237:999",
+    "190.71.82.182:999"
 ]
 
+# El emprendimiento del novio / El onlyfans XD
 # The info that the server will see
 # You can change the User-Agent to simulate different browsers
 # You can also add more headers if needed
@@ -21,11 +52,11 @@ headers = {
 
 # URL of the site you want to visit
 # You can change this to the site you want to visit
-url = "https://tusitio.com"
+url = "https://www.google.com"
 
 # Number of times you want to visit the site
 # You can change this to the number of times you want to visit th site
-times = 10
+times = 100
 
 # Main loop to visit the site
 # This loop will run 'times' times
@@ -42,4 +73,4 @@ for i in range(times):
         print(f"[{i+1}] Visitó con proxy {proxy} - Código: {response.status_code}")
     except Exception as e:
         print(f"[{i+1}] Falló con proxy {proxy}: {e}")
-    time.sleep(random.uniform(2, 5))  # Espera aleatoria entre visitas
+    time.sleep(random.uniform(2, 5))  # Wait for a random time between 2 and 5 seconds
